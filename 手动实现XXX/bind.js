3162dbc380a566b72e36aa03b3846aa07874cee2
HEYAN123 <490817982@qq.com>
2019-08-28 16:18:56 +0800

    将子元素逆序排列，修正bind函数

--- 手动实现XXX/bind.js
+++ 手动实现XXX/bind.js
@@ -3,6 +3,7 @@
 // 但bind会创建一个新的函数，
 // 当这个新函数被调用时，bind的第一个参数将作为他运行时候的this，
 // 之后的一系列参数会在传递的实参前传入作为它的参数
+// 函数中的return除非返回的是个对象，否则通过new返回的是个this，指向空对象，空对象原型指向原函数的prototype。
 
 Function.prototype.bind = function(context) {
     // 检查xxx.bind中xxx是否是正确的函数类型
@@ -13,15 +14,18 @@ Function.prototype.bind = function(context) {
     let self = this;
     // 取到后面的参数列表
     let args = [...arguments].slice(1);
+    // 创建一个空对象并将这个空对象的原型指向函数的原型
     function fn() {};
     fn.prototype = this.prototype;
     // 构建新函数的参数和调用时传入的参数的拼接
     let bound = function() {
         // args是bind调用时候传入的参数，arguments是创建的新函数调用时传入的参数
         let res = [...args, ...arguments];
-        context = this instanceof fn ? this : context || this;
+        // this是个fn吗？是的话context就是this（bound），不是的话就绑定到上一层的this对象，如果上一层对象为空的话指向全局
+        context = this instanceof fn ? this : context || window;
         return self.apply(context, res);
     }
+    // bound.prototype = Object.create(this.prototype)
     bound.prototype = new fn();
     return bound;
 }
